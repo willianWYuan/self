ac90cf6c6c76bb25d68bbe1c274a3e3d306c74eb
zhangweiyuan <734138689@qq.com>
2018-11-22 17:49:24 +0800

    router

--- use/jqueryrouter/router.js
+++ use/jqueryrouter/router.js
@@ -3,78 +3,59 @@ class createRouter {
         routes
     }) {
         this.routes = routes
+        this.history = [];
     }
-
-    params(argumentStr) {
-        let href = location.href.split('?')[1];
-        if (!href) return;
-        href = href.split(/&+/g);
-        let newObj = {};
-        for (let i = 0; i < href.length; i++) {
-            newObj[href[i].split('=')[0]] = href[i].split('=')[1]
-        }
-        return typeof argumentStr == 'undefined' ? newObj : newObj[argumentStr];
-    }
-
 
     log() {
-        console.log(`%c${this.page}:`, `color:#aaa`, Array.from(arguments).join(' '))
+        console.log(`%c${this.page}:`, `color:#aaa`, ...Array.from(arguments))
     }
 
 
     refresh() {
-        let [href, _self, currRouter, subRouterIndex] = [location.href.split('#')[1], this, null, 0];
-
-        // 重置参数
-        _self.done = null;
-        _self.params = {};
-
-
+        let [href, currRouter] = [location.href.split('#')[1], null];
         if (!href || href == '/') {
             location.href = `#${this.routes[0].path}`;
         } else {
-            // function isHasSubRouter(objective, nums) {   //判断是否有 子路由  返回有多少层子路由
-            //     if (objective.children) isHasSubRouter()
-            // }
-
-            _self.routes.map((item, itemIndex) => {
-                if (((isItemPath, isHref) => {
+            this.data = {};
+            this.params = {};
+            this.routes.map((item, itemIndex) => {
+                if (((isItemPathList, isHrefList) => {
                         let isflog;
-                        if (isItemPath.length == isHref.length) { // item.path和href  切割数量一样时
+                        if (isItemPathList.length == isHrefList.length) { // item.path和href  切割数量一样时
                             isflog = true;
-                            isItemPath.map((elem, elemIndex) => {
-                                if (elem.match(':')) this.params[elem.replace(':', '')] = isHref[elemIndex]; // 参数 不需要作对比  并记录参数值
-                                else if (elem != isHref[elemIndex]) isflog = false; // 只要有一个元素对不上  就不符合
+                            isItemPathList.map((elem, elemIndex) => {
+                                if (elem.match(':')) this.params[elem.replace(':', '')] = isHrefList[elemIndex]; // 参数 不需要作对比  并记录参数值
+                                else if (elem != isHrefList[elemIndex]) isflog = false; // 只要有一个元素对不上  就不符合
                             })
                         } else {
                             isflog = false;
                         }
                         return isflog;
-                    })(item.path.split('/'), href.split('/'))) _self.page = currRouter = item.url + '.html';
+                    })(item.path.split('/'), href.split('/'))) this.page = currRouter = item.url + '.html';
             });
-            console.log(currRouter)
-
-
-
-            $.ajax({
-                method: 'get',
-                url: currRouter,
-                cache: false,
-                success(res) {
-                    if (!currRouter) console.log('对应的url不存在/path没对应');
-
-                    $($('.router-view')[subRouterIndex]).html(currRouter ? res : '');
-                    try {
-                        _self.done()
-                    } catch (err) {
-                        // console.log(err);
-                    }
-                },
-                error(err) {
-                    console.error('获取文件失败')
-                }
-            });
+            this.include('.router-view', currRouter)
         }
+    }
+
+
+    include(elem, file) {
+        const _self = this;
+        $.ajax({
+            method: 'get',
+            url: file,
+            cache: false,
+            success(res) {
+                if (!file) console.log('对应的url不存在/path没对应');
+                $(elem).html(file ? res : '')
+                $('body').off('click').on('click', '[to]', function() {
+                    let to = $(this).attr('to');
+                    to.match(/\|/) ? _self.include(...to.split('|')) : location.href = '#' + to;
+                })  
+            },
+            error(err) {
+                console.error('获取文件失败')
+            }
+        });
     }
 }
 
@@ -92,13 +73,6 @@ window.router = new createRouter({
     }, {
         path: '/c',
         url: '/inner/c',
-        children: [{
-            path: '/c1',
-            url: '/inner/c1',
-        }, {
-            path: '/c2',
-            url: '/inner/c2',
-        }]
     }]
 })
 $(document).ready(() => router.refresh());
