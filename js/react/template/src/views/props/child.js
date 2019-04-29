7ec666fdfa6a84bcb2c0e9bd9a5515f55cfe5d0e
zhangweiyuan <734138689@qq.com>
2019-04-29 18:10:38 +0800

    0429

--- js/react/template/src/views/props/child.js
+++ js/react/template/src/views/props/child.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Child extends Component {
 
@@ -12,18 +13,32 @@ class Child extends Component {
     componentDidMount() {
         console.log(this)
     }
+    
 
 
     render() {
         let { childList, editClick, children } = this.props;
+        let { color } = this.context;
 
         return (
             <div className="child">
                 {children}  {/*相当于  <slot></slot>*/} 
                 {childList.map((item, itemIndex) => <span className="child-item" key={itemIndex} onClick={ev => editClick(itemIndex)}>{item}</span>)}
+                <br />
+                <br />
+                context传的值是color：{color}
+                <br />
+                <br />
             </div>
         );
     }
 }
+
+
+Child.contextTypes = {
+    color: PropTypes.string,
+    name: PropTypes.string
+};
+
 
 export default Child;
--- js/react/template/src/views/props/props.js
+++ js/react/template/src/views/props/props.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Child from './child';
+
+
 
 class PropsParent extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            childList: [1,2,3,4,5,6]
+            childList: [1,2,3,4,5,6],
+            color: "purple"
         }
 
     }
 
+    getChildContext() {
+        return {color: this.state.color, name: 'zwy'};
+    }
+
+
     componentDidMount() {
         console.log(this)
+    }
+    componentWillMount() {
+        console.log('componentWillMount', this.state.color)
     }
 
     editClick(index) {   // 生成原型方法     需要更改bind(this)
@@ -34,19 +46,28 @@ class PropsParent extends Component {
 
 
     render() {
-        let { childList } = this.state;
+        let { childList, color } = this.state;
         let { editClick } = this;
 
         return (
             <div className="props">
-                <Child childList={childList} editClick={editClick.bind(this)}  ref="t1">
+                <Child childList={childList} editClick={editClick.bind(this)} {...this.props} ref="t1">
                     {/*props/children <slot></slot> */}
                     <span className="child-title">数组传入子组件渲染：</span>
                 </Child>
-                <div className="text"  ref="t2">text</div>
+                <div className="text"  ref="t2" onClick={ev => this.setState({color: ev.target.innerHTML})}>text</div>
             </div>
         );
     }
 }
+
+PropsParent.childContextTypes = {
+    color: PropTypes.string,
+    name: PropTypes.string
+};
+// getChildContext() {
+//     return {color: "purple"};
+// }
+
 
 export default PropsParent;
